85c551af88e9816bfccf73f41884ad9d1608a20d
Kostya <pererva.kostya@outlook.com>
2020-03-02 02:15:37 +0200

    Commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,14 +1,25 @@
 import {openTabTemplate_1,openTabTemplate_2,openTabTemplate_3,openTabGallery,openPopup} from "./common";
 
 let btn = document.querySelector('.button-open-popup');
-let btnOpenRightPopup = document.querySelector('.button-open-popup-right');
+let btnOpenTab = document.querySelector('.button-open-popup-right');
 
-btn.addEventListener('click', function () {
+function openNewTab(popup) {
+    let openOtherTabLeft = document.querySelector('.open-other-popup-left');
+
+    openOtherTabLeft.addEventListener('click', () => {
+       popup.addNewTab(openTabTemplate_2);
+    });
+}
+
+
+btn.addEventListener('click', () => {
    let popup = openPopup(openTabGallery);
     popup.openWrapperForTabs();
     popup.addNewTab(openTabTemplate_3);
+    openNewTab(popup);
 });
 
-btnOpenRightPopup.addEventListener('click', function () {
-    openPopup(openTabTemplate_2, openTabTemplate_3)
+btnOpenTab.addEventListener('click', () => {
+    let popup = openPopup(openTabTemplate_2, openTabTemplate_3);
+    popup.openWrapperForTabs();
 });
